fc89935ea98482530a1b12aa5bd9e5830a83d62e
unknown <saritha.sunkari@archents.com>
2023-09-11 17:23:58 +0530

    modules creation

--- src/app/Modules/user/user-routing.module.ts
+++ src/app/Modules/user/user-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '', component: UserComponent,
+  //   children: [
+  //       { path: 'signIn', component: SigninComponent },
+  //       { path: 'login', component: LoginComponent },
+  //   ]
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
