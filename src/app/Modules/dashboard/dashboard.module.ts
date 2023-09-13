3bc4220f3cd13a9ed7e27fcf62c1ebac07824ecd
unknown <saritha.sunkari@archents.com>
2023-09-13 17:58:47 +0530

    add-product

--- src/app/Modules/dashboard/dashboard.module.ts
+++ src/app/Modules/dashboard/dashboard.module.ts
@@ -4,22 +4,25 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { HeaderComponent } from 'src/app/components/dashboard/header/header.component';
-import { AddProductComponent } from 'src/app/components/dashboard/add-product/add-product.component';
 import { MaterialModule } from '../material.module';
 import { NotificationsComponent } from 'src/app/components/dashboard/notifications/notifications.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddProductComponent } from 'src/app/components/dashboard/add-product/add-product.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,  
     HeaderComponent,
-    AddProductComponent,
-    NotificationsComponent
+    NotificationsComponent,
+    AddProductComponent
   ],
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class DashboardModule { }
--- src/app/components/dashboard/add-product/add-product.component.spec.ts
+++ src/app/components/dashboard/add-product/add-product.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddProductComponent } from './add-product.component';
+import { AddProductComponent } from './AddProductComponent';
 
 describe('AddProductComponent', () => {
   let component: AddProductComponent;
